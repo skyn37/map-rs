f432e0bc77999b73ff7f865983bd15625b1a1ed2
skyn37 <name@noname.com>
2023-12-26 14:29:00 +0200

    WIP draw: barebones slice and dice for dirs with random color

--- src/draw.rs
+++ src/draw.rs
@@ -1,11 +1,14 @@
 use minifb::{Key, Window, WindowOptions, KeyRepeat};
 use std::cmp::min;
 use crate::DirInfo;
-use std::time::{SystemTime, Instant};
 use crate::random;
+use std::time::Instant;
 
-const WIDTH: usize = 650;
-const HEIGHT: usize = 650;
+
+use crate::random::generate_random_hex_color;
+
+const WIDTH: usize = 800;
+const HEIGHT: usize = 600;
 
 
 struct Line {}
@@ -69,17 +72,17 @@ impl Vector2 {
 }
 #[derive(Debug, Clone)]
 struct Rec2 {
+    name: String,
     x: usize,
     y: usize,
     width: usize,
     height: usize,
     color: u32,
-    name : String
 }
 
 impl Rec2 {
 
-   pub fn draw_rect(rec: &Rec2, buffer: &mut Vec<u32>, buffer_width: usize) {
+    pub fn draw_rect(rec: &Rec2, buffer: &mut Vec<u32>, buffer_width: usize) {
         for row in rec.y..min(rec.y + rec.height, HEIGHT) {
             for col in rec.x..min(rec.x + rec.width, buffer_width) {
                 let index = row * buffer_width + col;
@@ -88,47 +91,47 @@ impl Rec2 {
         }
     }
 
-   pub fn draw_rect_with_border(rec: &Rec2, buffer: &mut Vec<u32>, buffer_width: usize, border_color: u32, border_size: usize) {
-       // Draw filled rectangle
-       for row in rec.y..min(rec.y + rec.height, HEIGHT) {
-           for col in rec.x..min(rec.x + rec.width, buffer_width) {
-               let index = row * buffer_width + col;
-               buffer[index] = rec.color;
-           }
-       }
+    pub fn draw_rect_with_border(rec: &Rec2, buffer: &mut Vec<u32>, buffer_width: usize, border_color: u32, border_size: usize) {
+        // Draw filled rectangle
+        for row in rec.y..min(rec.y + rec.height, HEIGHT) {
+            for col in rec.x..min(rec.x + rec.width, buffer_width) {
+                let index = row * buffer_width + col;
+                buffer[index] = rec.color;
+            }
+        }
 
-       // Draw top border
-       for row in rec.y..rec.y + border_size {
-           for col in rec.x..min(rec.x + rec.width, buffer_width) {
-               let index = row * buffer_width + col;
-               buffer[index] = border_color;
-           }
-       }
+        // Draw top border
+        for row in rec.y..rec.y + border_size {
+            for col in rec.x..min(rec.x + rec.width, buffer_width) {
+                let index = row * buffer_width + col;
+                buffer[index] = border_color;
+            }
+        }
 
-       // Draw bottom border
-       for row in rec.y + rec.height - border_size..rec.y + rec.height {
-           for col in rec.x..min(rec.x + rec.width, buffer_width) {
-               let index = row * buffer_width + col;
-               buffer[index] = border_color;
-           }
-       }
+        // Draw bottom border
+        for row in rec.y + rec.height - border_size..rec.y + rec.height {
+            for col in rec.x..min(rec.x + rec.width, buffer_width) {
+                let index = row * buffer_width + col;
+                buffer[index] = border_color;
+            }
+        }
 
-       // Draw left border
-       for row in rec.y..min(rec.y + rec.height, HEIGHT) {
-           for col in rec.x..rec.x + border_size {
-               let index = row * buffer_width + col;
-               buffer[index] = border_color;
-           }
-       }
+        // Draw left border
+        for row in rec.y..min(rec.y + rec.height, HEIGHT) {
+            for col in rec.x..rec.x + border_size {
+                let index = row * buffer_width + col;
+                buffer[index] = border_color;
+            }
+        }
 
-       // Draw right border
-       for row in rec.y..min(rec.y + rec.height, HEIGHT) {
-           for col in rec.x + rec.width - border_size..rec.x + rec.width {
-               let index = row * buffer_width + col;
-               buffer[index] = border_color;
-           }
-       }
-   }
+        // Draw right border
+        for row in rec.y..min(rec.y + rec.height, HEIGHT) {
+            for col in rec.x + rec.width - border_size..rec.x + rec.width {
+                let index = row * buffer_width + col;
+                buffer[index] = border_color;
+            }
+        }
+    }
 }
 
 
@@ -144,122 +147,98 @@ pub fn noise(dir_info: DirInfo) {
         .unwrap_or_else(|e| {
             panic!("{}", e);
         });
- 
+
     window.limit_update_rate(Some(std::time::Duration::from_micros(33200)));
 
     // Create a buffer to store pixel data
     let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
-    //    let h = Vector2 { x: WIDTH / 2, y: 0, color: 0xFF0000 }; // vert start
-    //    let h1 = Vector2 { x: WIDTH / 2 , y: HEIGHT, color: 0xFF0000  }; // vert end
-    //                                                                     //
-    //    let v = Vector2 { x: 0, y:  HEIGHT / 2, color: 0xFF0000  }; // hor start
-    //    let v1 = Vector2 { x: WIDTH , y: HEIGHT / 2, color: 0xFF0000  }; // hor end
-    //
     let mut rec_vec = Vec::new();
-    let mut rec1 = Rec2{
-        x: 25,
-        y: 25,
-        width: 600,
-        height: 600,
-        color: 0x4A412A,
-        name: String::from("Root"),
-    };
-
-    prepare_recs(&dir_info, &rec1, &mut rec_vec, true);
-    println!("{rec_vec:?}");
+    treemap_recursive(&dir_info, (WIDTH / 2 ) - (600 / 2), (HEIGHT / 2) - (400 / 2 ), 600, 400, &mut rec_vec);
     println!("{}", rec_vec.len());
     let mut step = 0;
+    let MAX_STEP: usize = rec_vec.len() -1;
+    const MIN_STEP :usize = 0;
+    let flag = true; // set this to false for forward and backward step DEBUGGING
     // Main loop
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        //    buffer.iter_mut().for_each(|pixel| *pixel = 0);
-        //    for rec in &rec_vec {
-        Rec2::draw_rect_with_border(&rec_vec[step], &mut buffer, WIDTH, 0xffffff, 1);
-        //    }
-
-        if window.is_key_pressed(Key::Space, KeyRepeat::No) {
-            if step == rec_vec.len() -1 {
-                step = rec_vec.len() -2;
+        if flag {
+            buffer.iter_mut().for_each(|pixel| *pixel = 0);
+            for rec in &rec_vec {
+                Rec2::draw_rect_with_border(rec, &mut buffer, WIDTH, 0xffffff, 1);
             }
-            Rec2::draw_rect_with_border(&rec_vec[step], &mut buffer, WIDTH, 0xffffff, 1);
-            step +=1;
+        } else {
+            if window.is_key_pressed(Key::Space, KeyRepeat::No) {
 
+                println!("{} {} \n", rec_vec[step].name, MAX_STEP);
+                Rec2::draw_rect_with_border(&rec_vec[step], &mut buffer, WIDTH, 0x21FC0D, 5);
+                if step == MAX_STEP {
+                    step = MAX_STEP;
+                } else {
+                    step +=1;
 
-        }
-        if window.is_key_pressed(Key::C, KeyRepeat::No) {
-            Rec2::draw_rect_with_border(&rec_vec[step], &mut buffer, WIDTH, 0xffffff, 1);
-            if step == 0 {
-                step = 0;
-            } else {
-
-                step -=1;
+                }
             }
+            if window.is_key_pressed(Key::C, KeyRepeat::No) {
+                println!("{}  {}\n", rec_vec[step].name, MAX_STEP);
 
-
-        }
-
-
-
+                Rec2::draw_rect_with_border(&rec_vec[step], &mut buffer, WIDTH, 0xffffff, 5);
+                if step == MIN_STEP {
+                    step = 0;
+                } else {
+                    step -=1;
+                }
+            }
+        } 
         window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
     }
 }
 
-    fn prepare_recs(dir_info: &DirInfo, parent_rec: &Rec2, recs: &mut Vec<Rec2>, start: bool) {
-        // let mut current_rec = parent_rec.clone();
-        // recs.push(current_rec.clone());
 
-        recs.push(parent_rec.clone());
-        let mut flag = !start;
+fn treemap_recursive(
+    dir_info: &DirInfo,
+    x: usize,
+    y: usize,
+    width: usize,
+    height: usize,
+    result: &mut Vec<Rec2>,
+    ) {
+    if dir_info.size > 0 {
+        let color = generate_random_hex_color(); 
+        let rec = Rec2 {
+            x,
+            y,
+            width,
+            height,
+            color,
+            name: dir_info.name.clone(),
+        };
+        result.push(rec);
+    }
+
+    let total_size: u64 = dir_info.children.iter().map(|child| child.size as u64).sum();
+    let children_len = dir_info.children.len();
+    if children_len % 2 == 0 {
+        let mut current_x = x;
 
         for child in &dir_info.children {
-           let mut modified_rec = parent_rec.clone();
-            if start {
-                modified_rec.height = ((modified_rec.height as f64 * ( child.percent_of_total / 100.0 )) as usize);  
-                if modified_rec.height == 0 {
-                    modified_rec.height = 20
-                }
-                modified_rec.color = generate_random_hex_color();
-
-            } 
-            if !start {
-                modified_rec.width = ((modified_rec.width as f64 * ( child.percent_of_total / 100.0 )) as usize);
-                if modified_rec.width == 0 {
-                    modified_rec.width = 20
-                }
-                modified_rec.color = generate_random_hex_color();
-
+            if child.size == 0 {
+                continue; // Skip zero-size directories
             }
- 
-            prepare_recs(child, &modified_rec, recs, flag);
+            let child_width = ((x + width - current_x) as f64 * (child.size as f64 / total_size as f64)) as usize;
+            treemap_recursive(child, current_x, y, child_width, height, result);
+            current_x += child_width;
         }
-    }
+    } else {
+        let mut current_y = y;
 
-
-
-
-
-fn generate_random_hex_color() -> u32 {
-    let seed = Instant::now().elapsed().subsec_nanos() as u32;
-    let mut rng = Lcg::new(seed);
-
-    rng.next()
-}
-
-
-struct Lcg {
-    state: u32,
-}
-
-impl Lcg {
-    fn new(seed: u32) -> Self {
-        Lcg { state: seed }
-    }
-
-    fn next(&mut self) -> u32 {
-        const A: u32 = 1664525;
-        const C: u32 = 1013904223;
-
-        self.state = self.state.wrapping_mul(A).wrapping_add(C);
-        self.state
+        for child in &dir_info.children {
+            if child.size == 0 {
+                continue; // Skip zero-size directories
+            }
+            let child_height = ((y + height - current_y) as f64 * (child.size as f64 / total_size as f64)) as usize;
+            treemap_recursive(child, x, current_y, width, child_height, result);
+            current_y += child_height;
+        }
     }
 }
 
